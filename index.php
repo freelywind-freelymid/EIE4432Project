910886214c63cbb8c7442a89908e0d4e7a93a5f7
freelywind-freelymid <ken.12330@yahoo.com.hk>
2021-12-04 17:49:01 +0800

    fixed the php bug which will show the msgs

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <?php
     session_start();
 
-    if($_SESSION['loginFlag'] == 'T'){
-        switch ($_SESSION['loginState']){
-            case 'A':
-            case 'C':
-                echo '<script>alert("Login succes!")</script>';
-                break;
-
-            case 'U':
-                echo '<script>alert("Login fail!")</script>';
-                break;
-            
-            default:
-                break;
+    if(isset($_SESSION['loginFlag']) && isset($_SESSION['loginState'])){
+        if($_SESSION['loginFlag'] == 'T'){
+            switch ($_SESSION['loginState']){
+                case 'A':
+                case 'C':
+                    echo '<script>alert("Login succes!")</script>';
+                    break;
+    
+                case 'U':
+                    echo '<script>alert("Login fail!")</script>';
+                    break;
+                
+                default:
+                    break;
+            }
+    
+            $_SESSION['loginFlag'] = 'F';
         }
-
-        $_SESSION['loginFlag'] = 'F';
     }
+    
 ?>
 
 <?php
-    session_start();
-
     //db setting
     $server = "localhost";
     $user = "eie4432project";
@@ -72,67 +73,56 @@
 <link rel="stylesheet" href="global.css">
 <link rel="stylesheet" href="index.css">
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="index.js"></script>
 </head>
 
 <body>  
-    <table>
-        <tr>
-            <td>
-                <div class="header">
-                    <?php include 'header.php'?>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <div class="promote">
-                    <img src="res/image/ui/promote.jpg">
-                    <div class="promote-word">
-                        <h1>What's <a id="next" href="#browsing">NEXT</a>?</h1>
-                    </div>
-                </div>
-            </td>
-        </tr>
-        <tr>        
-            <td>
-                <a name="browsing"></a>
-                <div class="item_page" id="item_page">
-                    <?php
-                        $items = $_SESSION['items'];
+    <div class="header">
+        <?php include 'header.php'?>
+    </div>
 
-                        foreach($items as $item){
-                            print "<div class=\"item\">";
+    <div class="promote">
+        <div class="promote-banner">
+            <img src="res/image/ui/promote.jpg">
+            <div class="promote-word">
+                <h1>What's <a id="next" href="#browsing">NEXT</a>?</h1>
+            </div>
+        </div>    
+    </div>
 
-                            print "<div class=\"item_upper\">";
-                            print "<img class=\"item_img\" src=\"".$item[3]."\">";
-                            print "<div class=\"item_butts\">";
-                            print "<button class=\"butt_buy\" value=\"".$item[0]."\">Buy now</button>";
-                            print "<button class=\"butt_addToCart\" value=\"".$item[0]."\">Add to cart</button>";
-                            print "</div>";
-                            print "</div>";
+    <a name="browsing"></a>
+    <div class="item_page" id="item_page">
+        <?php
+            $items = $_SESSION['items'];
 
-                            print "<div class=\"item_lower\">";
-                            print "<p>";
-                            print $item[1];
-                            print "<br>";
-                            print "<b>$".$item[2]."</b>";
-                            print "</p>";
-                            print "</div>";
+            foreach($items as $item){
+                print "<div class=\"item\">";
 
-                            print "</div>";
-                        }
-                    ?>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <div class="footer">
-                    <?php include 'footer.php'?>
-                </div>
-            </td>
-        </tr>
-    </table>
+                print "<div class=\"item_upper\">";
+                print "<img class=\"item_img\" src=\"".$item[3]."\">";
+                print "<div class=\"item_butts\">";
+                print "<button class=\"butt_buy\" value=\"".$item[0]."\">Buy now</button>";
+                print "<button class=\"butt_addToCart\" value=\"".$item[0]."\">Add to cart</button>";
+                print "</div>";
+                print "</div>";
+
+                print "<div class=\"item_lower\">";
+                print "<p>";
+                print $item[1];
+                print "<br>";
+                print "<b>$".$item[2]."</b>";
+                print "</p>";
+                print "</div>";
+
+                print "</div>";
+            }
+        ?>
+    </div>
+    
+    <div class="footer">
+        <?php include 'footer.php'?>
+    </div>
 <body>
 
 </html>
